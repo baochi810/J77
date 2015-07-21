76f46c64bf929ddafb6369a965d70bc4cb63096e
baochi810 <npgbchi@gmail.com>
2015-07-22 03:35:22 +0700

    add google analytic android

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -26,7 +26,171 @@ THE SOFTWARE.
 ****************************************************************************/
 package org.cocos2dx.cpp;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
+import org.cocos2dx.plugin.FacebookWrapper;
+import org.cocos2dx.plugin.PluginWrapper;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class AppActivity extends Cocos2dxActivity {
+	private static AppActivity _appActiviy;
+	private AdView adView;
+	private static final String AD_UNIT_ID = "ca-app-pub-3897960758510166/6316090337";
+
+	// Helper get display screen to avoid deprecated function use
+	private Point getDisplaySize(Display d) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			return getDisplaySizeGE11(d);
+		}
+		return getDisplaySizeLT11(d);
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private Point getDisplaySizeGE11(Display d) {
+		Point p = new Point(0, 0);
+		d.getSize(p);
+		return p;
+	}
+
+	private Point getDisplaySizeLT11(Display d) {
+		try {
+			Method getWidth = Display.class.getMethod("getWidth", new Class[] {});
+			Method getHeight = Display.class.getMethod("getHeight", new Class[] {});
+			return new Point(((Integer) getWidth.invoke(d, (Object[]) null)).intValue(),
+					((Integer) getHeight.invoke(d, (Object[]) null)).intValue());
+		} catch (NoSuchMethodException e2) // None of these exceptions should
+											// ever occur.
+		{
+			return new Point(-1, -1);
+		} catch (IllegalArgumentException e2) {
+			return new Point(-2, -2);
+		} catch (IllegalAccessException e2) {
+			return new Point(-3, -3);
+		} catch (InvocationTargetException e2) {
+			return new Point(-4, -4);
+		}
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		int width = getDisplaySize(getWindowManager().getDefaultDisplay()).x;
+
+		LinearLayout.LayoutParams adParams = new LinearLayout.LayoutParams(width,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.BANNER);
+		adView.setAdUnitId(AD_UNIT_ID);
+
+		AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+				.addTestDevice("4A6DAC31B6CF2AD5254B7D34893F6F0E").build();
+
+		adView.loadAd(adRequest);
+		adView.setBackgroundColor(Color.BLACK);
+		adView.setBackgroundColor(0);
+		addContentView(adView, adParams);
+
+		_appActiviy = this;
+
+
+	}
+
+	public static void hideAd() {
+		_appActiviy.runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				if (_appActiviy.adView.isEnabled())
+					_appActiviy.adView.setEnabled(false);
+				if (_appActiviy.adView.getVisibility() != 4)
+					_appActiviy.adView.setVisibility(View.INVISIBLE);
+			}
+		});
+
+	}
+
+	public static void showAd() {
+		_appActiviy.runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!_appActiviy.adView.isEnabled())
+					_appActiviy.adView.setEnabled(true);
+				if (_appActiviy.adView.getVisibility() == 4)
+					_appActiviy.adView.setVisibility(View.VISIBLE);
+			}
+		});
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (adView != null) {
+			adView.resume();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		if (adView != null) {
+			adView.pause();
+		}
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		adView.destroy();
+		super.onDestroy();
+
+	}
+
+	@Override
+	public Cocos2dxGLSurfaceView onCreateView() {
+		Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
+		// TestCpp should create stencil buffer
+		glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+
+		PluginWrapper.init(this);
+		PluginWrapper.setGLSurfaceView(glSurfaceView);
+		FacebookWrapper.onCreate(this);
+
+		return glSurfaceView;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (!PluginWrapper.onActivityResult(requestCode, resultCode, data)) {
+			super.onActivityResult(requestCode, resultCode, data);
+		}
+		FacebookWrapper.onAcitivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		FacebookWrapper.onSaveInstanceState(outState);
+	}
 }
